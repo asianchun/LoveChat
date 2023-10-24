6392ad54501994a314fa8631d13e009e1f992405
HongYuYi <yhon0009@student.monash.edu>
2023-10-24 12:40:52 +1100

    Connect the app to database

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@mern-tute.t2j4czv.mongodb.net/?retryWrites=true&w=majority"
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import express from "express"
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose"
 
 const app = express()
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`)
-})
+//Connect the app to the database
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database")
+    //Creates a server for us to use
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
