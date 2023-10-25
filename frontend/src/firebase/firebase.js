07b80d37f1d690c9b1ac847b31c89a00b945f953
HongYuYi <yhon0009@student.monash.edu>
2023-10-26 00:10:11 +1100

    init firebase, login, signup pages

---
+++ frontend/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkBloK4n2_MQf68VlfCllOiCjFdUhtBxE",
+  authDomain: "chat-app-6a1b0.firebaseapp.com",
+  projectId: "chat-app-6a1b0",
+  storageBucket: "chat-app-6a1b0.appspot.com",
+  messagingSenderId: "724622925027",
+  appId: "1:724622925027:web:07944714dc4d8af06539d6",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export default app
