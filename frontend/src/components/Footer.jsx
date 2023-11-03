19015578ed4eb0e634cd81fccd2c708a0fe69741
HongYuYi <yhon0009@student.monash.edu>
2023-11-03 11:56:34 +1100

    footer complete

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,27 +1,32 @@
 import { footerLogo } from "../assets"
+import { AiOutlineCopyrightCircle } from "react-icons/ai"
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 sm:px-16 px-8 py w-full">
-      <ul className="flex justify-between items-center font-palanquin">
-        <li>
-          <a href="#">
-            <img src={footerLogo} alt="logo" width={100} height={100} />
-          </a>
+    <footer className="absolute bottom-0 w-full">
+      <div className="flex items-center justify-center">
+        <img src={footerLogo} alt="logo" width={100} height={100} />
+        <span className="flex justify-center items-center relative right-6">
+          <AiOutlineCopyrightCircle /> 2023
+        </span>
+      </div>
+      <ul className="font-palanquin grid grid-cols-6 gap-2 mb-6 max-sm:mb-2 text-center max-sm:grid-cols-2 max-lg:grid-cols-3">
+        <li className="footer-link">
+          <a href="www.google.com.au">About</a>
         </li>
-        <li>
-          <a href="#">About</a>
+        <li className="footer-link">
+          <a href="www.google.com.au">Accessibility</a>
         </li>
-        <li>
+        <li className="footer-link">
           <a href="#">Privacy Policy</a>
         </li>
-        <li>
+        <li className="footer-link">
           <a href="#">Cookie Policy</a>
         </li>
-        <li>
+        <li className="footer-link">
           <a href="#">Copyright Policy</a>
         </li>
-        <li>
+        <li className="footer-link">
           <a href="#">Community Guidelines</a>
         </li>
       </ul>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        pink: "#ff66c4",
+      },
       fontFamily: {
         palanquin: ["Palanquin", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
