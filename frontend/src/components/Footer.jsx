73790c71eb2f0e89ae986c4c6aa2771d20ad6179
HongYuYi <yhon0009@student.monash.edu>
2023-11-05 19:58:14 +1100

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,13 +4,15 @@ import { AiOutlineCopyrightCircle } from "react-icons/ai"
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full">
-      <div className="flex items-center justify-center">
-        <img src={footerLogo} alt="logo" width={100} height={100} />
-        <span className="flex justify-center items-center relative right-6">
-          <AiOutlineCopyrightCircle /> 2023
-        </span>
-      </div>
-      <ul className="font-palanquin grid grid-cols-6 gap-2 mb-6 max-sm:mb-2 text-center max-sm:grid-cols-2 max-lg:grid-cols-3">
+      <ul className="font-palanquin grid grid-cols-6 gap-2 auto-rows-fr text-center mb-3 max-lg:mb-1 max-sm:grid-cols-2 max-lg:grid-cols-3 justify-center items-center">
+        <li className="footer-link">
+          <div className="flex gap-7 items-center justify-center">
+            <img src={footerLogo} alt="logo" width={30} height={30} />
+            <span className="flex justify-center items-center relative right-6">
+              <AiOutlineCopyrightCircle /> 2023
+            </span>
+          </div>
+        </li>
         <li className="footer-link">
           <a href="#">About</a>
         </li>
@@ -19,9 +21,6 @@ const Footer = () => {
         </li>
         <li className="footer-link">
           <a href="#">Privacy Policy</a>
-        </li>
-        <li className="footer-link">
-          <a href="#">Cookie Policy</a>
         </li>
         <li className="footer-link">
           <a href="#">Copyright Policy</a>
