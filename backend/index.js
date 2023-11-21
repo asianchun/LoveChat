b69aa2d77c0c026bc08182c531d933290972f5bc
HongYuYi <yhon0009@student.monash.edu>
2023-11-21 16:51:45 +1100

    changed cors origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-front-ep51.onrender.com/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
